99191d25439388d31cd5feaf728fcf680f46cf28
JerryMissTom <582486638@qq.com>
2018-06-28 09:30:51 +0800

    修复自定义组件在懒加载界面无法显示的Bug

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,8 @@
 import { IonicModule } from 'ionic-angular';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 // 解决ngFor报错
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 
 import { PaymentComponent } from './payment/payment';
@@ -14,8 +14,8 @@ import { FundinfoComponent } from './fundinfo/fundinfo';
         PaymentComponent
     ],
     imports: [
-        IonicModule,
-        BrowserModule
+      //  IonicModule, 假如你的自定义组件中包含表单元素，还需要引入  FormsModule, ReactiveFormsModule,
+        CommonModule
     ],
     exports: [
         FundinfoComponent,
